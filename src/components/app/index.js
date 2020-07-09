aebb4aa1c05c7b85e7eb20870218e11ea6457161
valentinEutrope <valentin.eutrope@free.fr>
2020-07-09 21:21:18 +0200

    implement player with collision, animation and control keys

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -40,7 +40,12 @@ export default class App {
     }
 
     function create() {
+      // Background
+
       this.add.image(frame.width / 2, frame.height / 2, "sky");
+
+      // Platform
+
       var platforms = this.physics.add.staticGroup();
 
       platforms.create(400, 568, "ground").setScale(2).refreshBody();
@@ -48,8 +53,58 @@ export default class App {
       platforms.create(600, 400, "ground");
       platforms.create(50, 250, "ground");
       platforms.create(750, 220, "ground");
+
+      // Player
+
+      var player = this.physics.add.sprite(100, 450, "dude");
+
+      player.setBounce(0.2);
+      player.setCollideWorldBounds(true);
+      player.body.setGravityY(300);
+
+      this.physics.add.collider(player, platforms);
+
+      this.anims.create({
+        key: "left",
+        frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1,
+      });
+
+      this.anims.create({
+        key: "turn",
+        frames: [{ key: "dude", frame: 4 }],
+        frameRate: 20,
+      });
+
+      this.anims.create({
+        key: "right",
+        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+      });
     }
 
-    function update() {}
+    function update() {
+      var cursors = this.input.keyboard.createCursorKeys();
+
+      if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+
+        player.anims.play("left", true);
+      } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play("right", true);
+      } else {
+        player.setVelocityX(0);
+
+        player.anims.play("turn");
+      }
+
+      if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+      }
+    }
   }
 }
